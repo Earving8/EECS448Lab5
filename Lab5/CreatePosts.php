7bafa0767b7e3f583d0811010899f76b9ac37dd8
Earving <earving_08@hotmail.com>
2019-04-05 10:26:31 -0500

    created create posts

---
+++ Lab5/CreatePosts.php
@@ -0,0 +1,37 @@
+
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+
+$user = $_POST["userName"];
+$content = $_POST["content"];
+$mysqli = new mysqli("mysql.eecs.ku.edu", "j666m000", "gei4uiYi", "j666m000");
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+
+$existing = "SELECT user_id from Users where user_id ='$user'";
+$post = "INSERT INTO Posts (content, author_id) VALUES ('$content','$user')";
+
+
+if (mysqli_num_rows($mysqli->query($existing)) === 1 ){
+    
+    if($mysqli->query($post)){
+        echo "Posted succesfully";
+    }else{
+        echo "unable to post";
+    }
+
+}else{
+    echo "User does not exist";
+}
+
+/* close connection */
+
+$mysqli->close();
+?>
