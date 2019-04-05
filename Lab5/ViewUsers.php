4a01bd9c74542c98511ba9ca2daa238cd4b16ba7
Earving <earving_08@hotmail.com>
2019-04-05 10:39:37 -0500

    created admin page and view users

---
+++ Lab5/ViewUsers.php
@@ -0,0 +1,31 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+
+$mysqli = new mysqli("mysql.eecs.ku.edu", "j666m000", "gei4uiYi", "j666m000");
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+
+$query = "SELECT * from Users";
+
+
+if ($result = $mysqli->query($query)){
+    
+    while ($row = $result->fetch_assoc()) {
+        echo $row['user_id']."<br>";
+    }
+
+}else{
+    echo "error";
+}
+
+/* close connection */
+
+$mysqli->close();
+?>
