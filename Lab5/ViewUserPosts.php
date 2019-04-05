0df6565d0fef2ddced2a8c7bb71e30a7f54ed0af
Earving <earving_08@hotmail.com>
2019-04-05 11:13:55 -0500

    added viewing of posts from user

---
+++ Lab5/ViewUserPosts.php
@@ -0,0 +1,35 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+
+$mysqli = new mysqli("mysql.eecs.ku.edu", "j666m000", "gei4uiYi", "j666m000");
+$user = $_POST["userName"];
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+
+$query = "SELECT * from Posts where author_id='$user'";
+
+
+if ($result = $mysqli->query($query)){
+    if(mysqli_num_rows($result) === 0){
+        echo "No posts from this user";
+    }else{
+        while ($row = $result->fetch_assoc()) {
+            echo $row['content']."<br>";
+        }  
+    }
+
+}else{
+    echo "error";
+}
+
+/* close connection */
+
+$mysqli->close();
+?>
